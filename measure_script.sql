f808ed289844e32e073d424941f31eb6ebaeac31
lockerz1290 <51370230+lockerz1290@users.noreply.github.com>
2025-03-28 08:34:55 +0100

    Update measure_script.sql

--- measure_script.sql
+++ measure_script.sql
@@ -6,7 +6,7 @@ DECLARE
   v_time_query2 INTERVAL DAY TO SECOND;
   
   -- Licznik pętli
-  v_iterations NUMBER := 10; -- Liczba iteracji do wykonania
+  v_iterations NUMBER := 5; -- Liczba iteracji do wykonania
   
   -- Statystyki
   v_total_time_query1 INTERVAL DAY TO SECOND := INTERVAL '0' SECOND;
@@ -14,11 +14,27 @@ DECLARE
   v_avg_time_query1 INTERVAL DAY TO SECOND;
   v_avg_time_query2 INTERVAL DAY TO SECOND;
   
-  -- Wyniki (jeśli zapytania zwracają dane)
-  v_result1 NUMBER;
-  v_result2 NUMBER;
+  -- Kursor dla wyników pierwszego zapytania
+  CURSOR c_query1 IS
+    -- TU WPISZ PIERWSZE ZAPYTANIE (zwracające wszystkie kolumny)
+    -- Przykład:
+    SELECT * FROM employees WHERE department_id = 10;
+    
+  -- Kursor dla wyników drugiego zapytania
+  CURSOR c_query2 IS
+    -- TU WPISZ DRUGIE ZAPYTANIE (zwracające wszystkie kolumny)
+    -- Przykład:
+    SELECT * FROM employees WHERE department_id = 20;
+    
+  -- Rekordy do przechowywania wyników (dla przykładu)
+  r_query1 c_query1%ROWTYPE;
+  r_query2 c_query2%ROWTYPE;
+  
+  -- Liczniki rekordów
+  v_count1 NUMBER := 0;
+  v_count2 NUMBER := 0;
 BEGIN
-  -- Nagłówek wyników
+  -- Nagłówek wyników czasowych
   DBMS_OUTPUT.PUT_LINE('Iteracja | Zapytanie 1 Czas | Zapytanie 2 Czas | Różnica');
   DBMS_OUTPUT.PUT_LINE('-------------------------------------------------------');
   
@@ -27,9 +43,15 @@ BEGIN
     -- Wykonaj i zmierz pierwsze zapytanie
     v_start_time := SYSTIMESTAMP;
     
-    -- TU WPISZ PIERWSZE ZAPYTANIE (np. SELECT COUNT(*) INTO v_result1 FROM tabela1 WHERE warunek)
-    -- Przykład:
-    SELECT COUNT(*) INTO v_result1 FROM employees WHERE department_id = 10;
+    -- Otwórz kursor i przetwarzaj wyniki
+    OPEN c_query1;
+    LOOP
+      FETCH c_query1 INTO r_query1;
+      EXIT WHEN c_query1%NOTFOUND;
+      v_count1 := v_count1 + 1;
+      -- Tutaj możesz przetwarzać dane (r_query1.zmienna)
+    END LOOP;
+    CLOSE c_query1;
     
     v_end_time := SYSTIMESTAMP;
     v_time_query1 := v_end_time - v_start_time;
@@ -38,43 +60,47 @@ BEGIN
     -- Wykonaj i zmierz drugie zapytanie
     v_start_time := SYSTIMESTAMP;
     
-    -- TU WPISZ DRUGIE ZAPYTANIE (np. SELECT COUNT(*) INTO v_result2 FROM tabela2 WHERE warunek)
-    -- Przykład:
-    SELECT COUNT(*) INTO v_result2 FROM employees WHERE department_id = 20;
+    -- Otwórz kursor i przetwarzaj wyniki
+    OPEN c_query2;
+    LOOP
+      FETCH c_query2 INTO r_query2;
+      EXIT WHEN c_query2%NOTFOUND;
+      v_count2 := v_count2 + 1;
+      -- Tutaj możesz przetwarzać dane (r_query2.zmienna)
+    END LOOP;
+    CLOSE c_query2;
     
     v_end_time := SYSTIMESTAMP;
     v_time_query2 := v_end_time - v_start_time;
     v_total_time_query2 := v_total_time_query2 + v_time_query2;
     
-    -- Wyświetl wyniki dla tej iteracji
+    -- Wyświetl wyniki czasowe dla tej iteracji
     DBMS_OUTPUT.PUT_LINE(
       LPAD(i, 8) || ' | ' ||
       TO_CHAR(v_time_query1, 'HH24:MI:SS.FF3') || ' | ' ||
       TO_CHAR(v_time_query2, 'HH24:MI:SS.FF3') || ' | ' ||
       TO_CHAR(v_time_query2 - v_time_query1, 'HH24:MI:SS.FF3')
     );
-    
-    -- Opcjonalne czyszczenie bufora (dla niektórych operacji)
-    COMMIT;
   END LOOP;
   
   -- Oblicz średnie czasy
   v_avg_time_query1 := v_total_time_query1 / v_iterations;
   v_avg_time_query2 := v_total_time_query2 / v_iterations;
   
-  -- Podsumowanie
+  -- Podsumowanie czasowe
   DBMS_OUTPUT.PUT_LINE('-------------------------------------------------------');
   DBMS_OUTPUT.PUT_LINE('ŚREDNIA:');
   DBMS_OUTPUT.PUT_LINE('Zapytanie 1: ' || TO_CHAR(v_avg_time_query1, 'HH24:MI:SS.FF3'));
   DBMS_OUTPUT.PUT_LINE('Zapytanie 2: ' || TO_CHAR(v_avg_time_query2, 'HH24:MI:SS.FF3'));
   DBMS_OUTPUT.PUT_LINE('Różnica: ' || TO_CHAR(v_avg_time_query2 - v_avg_time_query1, 'HH24:MI:SS.FF3'));
   
-  -- Dodatkowe statystyki
+  -- Statystyki dotyczące liczby zwróconych rekordów
   DBMS_OUTPUT.PUT_LINE('-------------------------------------------------------');
-  DBMS_OUTPUT.PUT_LINE('Zapytanie 1 było szybsze w ' || 
-    CASE WHEN v_avg_time_query1 < v_avg_time_query2 THEN 'TAK' ELSE 'NIE' END || ' przypadkach');
-  DBMS_OUTPUT.PUT_LINE('Procentowa różnica: ' || 
-    ROUND(ABS((EXTRACT(SECOND FROM v_avg_time_query1) - EXTRACT(SECOND FROM v_avg_time_query2)) / 
-    GREATEST(EXTRACT(SECOND FROM v_avg_time_query1), EXTRACT(SECOND FROM v_avg_time_query2)) * 100, 2) || '%');
+  DBMS_OUTPUT.PUT_LINE('LICZBA ZWRÓCONYCH REKORDÓW:');
+  DBMS_OUTPUT.PUT_LINE('Zapytanie 1: ' || v_count1/v_iterations || ' (średnio na iterację)');
+  DBMS_OUTPUT.PUT_LINE('Zapytanie 2: ' || v_count2/v_iterations || ' (średnio na iterację)');
+  
+  -- Wyświetlenie przykładowych wyników (pierwszy rekord z ostatniej iteracji)
+  DBMS_OUTPUT.PUT_LINE('-------------------------------------------------------');
+  DBMS_OUTPUT.PUT_LINE('PRZYKŁADOWE WYNIKI (pierwszy rekord z ostatniej iteracji):');
 END;
-/
